7467b9dbef0d7b985772a1c135864ee06cf775ce
vaxevane <81081231+vaxevane@users.noreply.github.com>
2021-07-17 03:22:31 +0300

    Update Sobel.h

--- Sobel.h
+++ Sobel.h
@@ -3,6 +3,7 @@
 
 const int N = 288;
 const int M = 352;
+const int W = 3;
 
 void padding(float image[N][M]){
 	
@@ -18,7 +19,8 @@ void padding(float image[N][M]){
 	}
 }
 
-float convolution(float window[3][3],float kernel[3][3]){
+//float convolution(float window[N][M],float kernel[W][W]){
+float convolution(float window[W][W],float kernel[W][W]){
 	
 	int sum = 0;
 	for (int i = 0; i < 3; i++){
@@ -32,55 +34,44 @@ float convolution(float window[3][3],float kernel[3][3]){
 
 void Sobel_algorithm(float image[N][M], float Gx[N][M], float Gy[N][M], float edges[N][M],float angle[N][M]){
 
-	float kx[3][3] = {{0, 0, 0},{-2, 0, 2},{-1, 0, 1}};
-	float ky[3][3] = {{-1, -2, -1},{0, 0, 0},{1, 2, 1}};
+	float kx[W][W] = {{0, 0, 0},{-2, 0, 2},{-1, 0, 1}};
+	float ky[W][W] = {{-1, -2, -1},{0, 0, 0},{1, 2, 1}};
 	
-	float window[3][3]; 
+	float window[W][W]; 
 	typedef short linebuffer[M];
 	linebuffer lb[2];
+	
+	float tempx,tempy,sq_sum;
 	
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			
 			//Sliding window
-			window[0][0] = window[0][1];
-			window[1][0] = window[1][1];
-			window[2][0] = window[2][1];
-			
-			window[0][1] = window[0][2];
-			window[1][1] = window[1][2];
-			window[2][1] = window[2][2];
-			
-			window[0][2] = lb[0][j];
-			window[1][2] = (lb[0][j] = lb[1][j]);
-			window[2][2] = (lb[1][j] = image[i][j]);
-			
-		//	std::cout << i-1 << " " << j-1 << " wind :"; 
-			for (int a=0;a<3;a++){
-				for (int b=0;b<3;b++){
-				//	std::cout << window[a][b] << " ";
-				}
-				//std::cout << "\n";
+			for (int l=0; l < W; l++) {
+				window[0][l] = (l==W-1) ? lb[0][j] : window[0][l+1];
+				window[1][l] = (l==W-1) ? lb[1][j] : window[1][l+1];
+				window[2][l] = (l==W-1) ? image[i][j]: window[2][l+1];
 			}
+			lb[0][j]=lb[1][j];
+			lb[1][j]=image[i][j];	
+
 			if(i > 1 && j > 1){
-				Gx[i-1][j-1] = convolution(window, kx);
-				Gy[i-1][j-1] = convolution(window, ky);
-				float tempx = Gx[i-1][j-1];
-				float tempy = Gy[i-1][j-1];
-				edges[i-1][j-1] = sqrt(tempx*tempx + tempy*tempy);
-				angle[i-1][i-1] = atan2(tempx,tempy);	
+				tempx = (Gx[i-1][j-1] = convolution(window, kx));
+			    tempy =	(Gy[i-1][j-1] = convolution(window, ky));
+				sq_sum = tempx*tempx +tempy*tempy;
+				edges[i-1][j-1] = sqrt(sq_sum);
+				angle[i-1][j-1] = atan2(tempy,tempx);	
 			}			
 		}
 	}
 	
 /*	for(int i = 1; i < N-1; i++){
 		for(int j = 1; j< M-1; j++){
-			Gx[i][j] = convolution(image, kx, i, j);
-			Gy[i][j] = convolution(image, ky, i, j);
-			float tempx = Gx[i][j];
-			float tempy = Gy[i][j];
-			edges[i][j] = sqrt(tempx*tempx + tempy*tempy);
-			angle[i][i] = atan2(tempx,tempy);	
+			tempx = (Gx[i-1][j-1] = convolution(window, kx));
+			tempy = (Gy[i-1][j-1] = convolution(window, ky));
+			sq_sum = tempx*tempx +tempy*tempy;
+			edges[i-1][j-1] = sqrt(sq_sum);
+			angle[i-1][j-1] = atan2(tempy,tempx);	
 		}
 	}*/
 }
